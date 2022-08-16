43508f1428683bee47b7b8af62f863b7943d25a7
jfberner <jfredericoberner@gmail.com>
2022-08-16 16:07:56 -0300

    updating to use train and test data from 01_Occ_to_shp.R

--- R/03_Model-Build.R
+++ R/03_Model-Build.R
@@ -6,8 +6,6 @@
 # Author: João Frederico Berner
 # --------------------------------------------------#
 
-path <- '~/Documents/CharinusENM' # I'm so sorry about this, I really can't figure out a better way to make this work.
-
 # Libs #####
 # remotes::install_github(babaknaimi/sdm)
 library(sdm)
@@ -21,16 +19,12 @@ char2seed('Charinus')
 
 # Load Data #####
 ## Environmental Data #####
-setwd(path)
-setwd('data/processed/envcropped/Present/')
-envpres <- dir(pattern="tif$")
+envpres <- dir(path = 'data/processed/envcropped/Present/', pattern = ".tif$", full.names = T)
 envpres <- raster::stack(envpres)
-setwd(path)
 
 ## Occurrence Data #####
-occ_raw <- read.csv('data/raw/occ/presence_absence.csv')
-occ_pres <- occ_raw %>% filter(charinus == 1) # only presence data, dropping absences
-occ_abs <- occ_raw %>% filter(charinus == 0)
+occ_train <- readOGR('data/processed/shapefiles/train.shp') # only presence data, dropping absences
+occ_test <- readOGR('data/processed/shapefiles/test.shp')
 
 # Turn the object into a SpatialPoints dataframe
 coordinates(occ_pres) <- ~long + lat
@@ -39,12 +33,12 @@ proj4string(occ_pres) <- projection(raster())
 # Model Build #####
 
 d_occ <- sdmData(formula = charinus~., 
-                 train=occ_pres, predictors = envpres,
+                 train=occ_train, test = occ_test, predictors = envpres,
                  bg = 18, method = 'eRandom')
 
 m_occ <- sdm(formula = charinus~.,data = d_occ,
              methods = c('svm', 'maxent', 'brt', 'bioclim', 'domain'),
-             replication = c('cv'), n=10) 
+             replication = c('cv'), n=5) 
 
 # gui(m_occ)
 
